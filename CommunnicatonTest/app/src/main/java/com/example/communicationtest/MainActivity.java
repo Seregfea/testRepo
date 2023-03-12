f88478b9fc81238f5eec92927cf0eae4afef3f97
Seregfea <www.Valarauco@gmx.at>
2023-03-12 21:14:44 +0100

    order list complette
    works right the way it should now

--- CommunnicatonTest/app/src/main/java/com/example/communicationtest/MainActivity.java
+++ CommunnicatonTest/app/src/main/java/com/example/communicationtest/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.communicationtest;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText inputText;
     private TextView outputText;
+    private Button buttonStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
         inputText = findViewById(R.id.inputText);
         outputText = findViewById(R.id.outputTextView);
+        buttonStart = findViewById(R.id.button);
 
 
-        inputText.setOnClickListener(view -> {
-            Log.d("serverI", "-------------cklicked----------------");
+
+        buttonStart.setOnClickListener(view -> {
+            SortArray sort = new SortArray();
+            String orderedMar = sort.sortArray(Integer.parseInt(inputText.getText().toString()));
+            Log.d("serverI", "-------------cklicked----------------"+orderedMar);
             TCPClient task = new TCPClient();
+            outputText.setText(orderedMar);
             task.connect(outputText);
             try {
                 task.send(inputText.getText().toString());
--- CommunnicatonTest/app/src/main/java/com/example/communicationtest/SortArray.java
+++ CommunnicatonTest/app/src/main/java/com/example/communicationtest/SortArray.java
@@ -1,31 +1,48 @@
 package com.example.communicationtest;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SortArray {
-    int[] fullNumber;
+    private String fullNumber;
 
     private void intToString(int fullNumber){
-        String temp = Integer.toString(fullNumber);
-        this.fullNumber = new int[temp.length()];
+        this.fullNumber = Integer.toString(fullNumber);
+        Log.d("serverS2", "-------------cklickedeven----------------"+this.fullNumber);
     }
 
-    public List sortArray(int fullNumber){
+    private String listToString(List<Integer> listInt){
+        String listComp = "";
+        for (int i = 0; i < listInt.size(); i++) {
+            listComp = listComp+listInt.get(i);
+        }
+        Log.d("serverA3", "-------------cklickedoteven----------------"+listComp);
+
+        return listComp;
+    }
+
+    public String sortArray(int fullNumber){
         intToString(fullNumber);
         List<Integer> even = new ArrayList<>();
         List<Integer> notEven = new ArrayList<>();
         List<Integer> ordered = new ArrayList<>();
-        for (int i = 0; i< this.fullNumber.length; i++){
-            if((this.fullNumber[i] % 2) == 0){
-                even.add(this.fullNumber[i]);
-            }else {
-                notEven.add(this.fullNumber[i]);
+        int tester;
+        for(int i=0; i<this.fullNumber.length(); i++) {
+            tester = Integer.parseInt(this.fullNumber.charAt(i)+"");
+            Log.d("serverT1", "-------------cklickedeven----------------"+tester);
+            if ((tester % 2) == 0) {
+                even.add(tester);
+            } else {
+                notEven.add(tester);
             }
         }
+        Log.d("serverA1", "-------------cklickedeven----------------"+even.toString());
+        Log.d("serverA2", "-------------cklickedoteven----------------"+notEven.toString());
         ordered.addAll(even);
         ordered.addAll(notEven);
-        return ordered;
+        return listToString(ordered);
     }
 
 }
